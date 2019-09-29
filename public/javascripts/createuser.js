27825782ee39205771fb6a3e913491bf67cb092a
Swastika Library <rahul1.shc@gmail.com>
2019-09-29 18:31:44 +0530

    modified on click handler client side script for creating/deleting/updating a user

--- public/javascripts/createuser.js
+++ public/javascripts/createuser.js
@@ -1,65 +1,31 @@
 //update
-/*function update(obj){
-    //object id
-    //console.log(obj);
+function updateuser(obj){
+    
     var arr=obj.parentNode.parentNode.childNodes;
     //object id of the residency record
     var col0= arr[0].innerHTML;
-    //resident type
-    var col1= arr[1].innerHTML;
-    //object id if the resident
-    var col2= arr[2].innerText;
-    //order no
-    var col4= arr[4].innerHTML;
-    //date of order
-    var col5= arr[5].innerHTML;
-    //date of occupancy
-    var col6= arr[6].innerHTML;
-    //date of extend up to
-    var col7= arr[7].innerHTML;
-    //date of vacancy
-    var col8= arr[8].innerHTML;
-    //comments
-    var col9= arr[9].innerHTML;
+    var id=document.getElementById('objectid1');
+    id.value=col0;
 
-    //console.log(elem);
-    //employee/pensioner/other's name
-    var col10= arr[10].innerHTML;
+    document.getElementById('username1').value=arr[1].innerHTML;
+    document.getElementById('username1').disabled=true;
 
-    var id=document.getElementById('id');
-    var employeeid=document.getElementById('name');
-    var employee=document.getElementById('employee');
-    var pensioner=document.getElementById('pensioner');
-    var others=document.getElementById('others');
-    var signal=document.getElementById('signal');
-    var orderno=document.getElementById('orderno');
-    var orderdate=document.getElementById('orderdate');
-    var occupieddate=document.getElementById('occupieddate');
-    var extendupto=document.getElementById('extendupto');
-    var vacantdate=document.getElementById('vacantdate');
-    var comments=document.getElementById('comments');
-    //console.log(elem1.placeholder);
-    id3.value=col0;
-    id4.value=col2;
-    employeeid.value=col10;
-    employeeid.disabled=true;
-    employee.value=col1;
-    employee.innerHTML=col1;
-    pensioner.hidden=true;
-    others.hidden=true;
-    orderno.value=col4;
-    orderdate.value=col5;
-    occupieddate.value=col6;
-    extendupto.value=col7;
-    vacantdate.value=col8;
+    document.getElementById('empid1').value=arr[2].innerHTML;
+    
+    document.getElementById('mobilenumber1').value=arr[4].innerHTML;
+    
+    if(arr[5].innerHTML==='admin') document.getElementById('role1').value='admin';
+    else if (arr[5].innerHTML==='hr') document.getElementById('role1').value='hr';
+    else if (arr[5].innerHTML==='accounts') document.getElementById('role1').value='accounts';
 
-    signal.value='update';
+    if(arr[6].innerHTML==='active') document.getElementById('status1').value='active';
+    else if (arr[6].innerHTML==='inactive') document.getElementById('status1').value='inactive';
 
-    //console.log(orderdate.value);
-    comments.value=col9;
-    //console.log(elem1.placeholder);
+    document.getElementById('comments1').value=arr[7].innerHTML;
 
-}*/
+    document.getElementById('signal1').value='update';
+
+}
 
 function insertuser(){
 
@@ -68,23 +34,18 @@ function insertuser(){
 
 }
 
-/*function remove(obj){
+function removeuser(obj){
 
     var arr=obj.parentNode.parentNode.childNodes;
-    //console.log(arr[2].innerText);
-    //console.log(arr[2].innerHTML);
-    //object id of the residency record
+
+    //object id
     var col0= arr[0].innerHTML;
-    var col2= arr[2].innerHTML;
-    var id1=document.getElementById('id1');
-    var id2=document.getElementById('id2');
 
-    id1.value=col0;
-    id2.value=col2;
+    var id=document.getElementById('objectid2');
+    id.value=col0;
+    //console.log(id.value);
+    var signal2=document.getElementById('signal2');
+    signal2.value='delete';
 
-    //console.log(id2.value);
-    var signal1=document.getElementById('signal1');
-    signal1.value='delete';
-
-}*/
+}
 
