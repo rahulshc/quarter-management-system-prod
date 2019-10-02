d64e937ed6459769fcd59a7214e42ef95bf651c3
Swastika Library <rahul1.shc@gmail.com>
2019-10-02 21:42:34 +0530

    modified the prosixmaster model

--- models/prosixmaster.js
+++ models/prosixmaster.js
@@ -6,13 +6,12 @@ var ProsixmasterSchema= new Schema (
     {
         employeeid: { type:String, required: true},
         name: { type:String, required: true, max: 150},
-        designation: { type:String, required: true, max: 150},
+        designation: { type:String,  max: 150},
+        contactnumber: String,
         prev_company: { type:String,  max: 150},
         prev_location: { type:String,  max: 150},
         curr_company: { type:String,   max: 150},
         curr_location: { type:String,   max: 150},
-        prev_location_hra_type:{ type:String,  enum: ['city', 'others'], max: 150},
-        curr_location_hra_type:{ type:String,  enum: ['city', 'others'], max: 150},
         date_of_actual_retirement: Date,
         last_maint_charge_deducted_amount: Number
     }
